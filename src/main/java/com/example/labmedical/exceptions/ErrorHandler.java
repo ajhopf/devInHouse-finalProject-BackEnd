b69bfd2813a8bc20bc0cc457ef9c3194db56c707
Leandro-da Silva-Michail-Krikis <leandro_silva22@estudante.sesisenai.org.br>
2023-07-12 21:21:15 -0300

    Criacao do controller "LogController" e do endpoint "/api/logs/cadastrar"

--- src/main/java/com/example/labmedical/exceptions/ErrorHandler.java
+++ src/main/java/com/example/labmedical/exceptions/ErrorHandler.java
@@ -1,5 +1,8 @@
 package com.example.labmedical.exceptions;
 
+import com.example.labmedical.service.LogService;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -7,13 +10,18 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ErrorHandler {
+    @Autowired
+    private LogService logger;
     @ExceptionHandler(WrongCredentialsException.class)
     public ResponseEntity<String> entityNotFound(WrongCredentialsException e) {
+        logger.error("Entidade não encontrada " + e.getMessage());
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(e.getMessage());
@@ -27,5 +35,19 @@ public class ErrorHandler {
                 erros.stream()
                         .map(ValidationErrorDto::new)
                         .collect(Collectors.toList()));
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Map<String, Object>> trataErro400(ConstraintViolationException e) {
+        var retorno = new HashMap<String, Object>();
+        for(var validator : e.getConstraintViolations()) {
+            var key = "";
+            var value = validator.getMessage();
+            for(var node : validator.getPropertyPath()) {
+                key = node.getName();
+            }
+            retorno.put(key, value);
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(retorno);
     }
 }

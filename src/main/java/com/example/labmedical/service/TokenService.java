5170649663184733a9ae64d1d90b9bbd5b2fc9c9
André Hopf <andre.hopf@gmail.com>
2023-07-14 18:48:21 -0300

    003 - Adiciona testes para TokenService

--- src/main/java/com/example/labmedical/service/TokenService.java
+++ src/main/java/com/example/labmedical/service/TokenService.java
@@ -2,8 +2,10 @@ package com.example.labmedical.service;
 
 import com.example.labmedical.enums.TokenType;
 import com.example.labmedical.repository.TokenRepository;
+import com.example.labmedical.repository.UserRepository;
 import com.example.labmedical.repository.model.Token;
 import com.example.labmedical.repository.model.User;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,15 +13,20 @@ import org.springframework.stereotype.Service;
 public class TokenService {
     @Autowired
     private TokenRepository tokenRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     public void save(String token, User user) {
+        User existentUser = userRepository.findById(user.getId())
+                .orElseThrow(EntityNotFoundException::new);
+
         tokenRepository.save(
                 Token.builder()
                         .token(token)
                         .expired(false)
                         .revoked(false)
                         .tokenType(TokenType.BEARER)
-                        .user(user)
+                        .user(existentUser)
                         .build()
         );
     }

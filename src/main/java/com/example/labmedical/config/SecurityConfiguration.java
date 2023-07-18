a600d83ad99f007344c933bf445ed2ed837ad4b6
André Hopf <andre.hopf@gmail.com>
2023-07-18 11:04:04 -0300

    Adiciona configuração para autenticação com jwt

--- src/main/java/com/example/labmedical/config/SecurityConfiguration.java
+++ src/main/java/com/example/labmedical/config/SecurityConfiguration.java
@@ -1,22 +1,49 @@
 package com.example.labmedical.config;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfiguration {
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+    private final LogoutHandler logoutHandler;
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
+        http
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("**")
-                .permitAll().and().build();
+                .requestMatchers("/api/usuarios/login", "/swagger-ui/**", "/v3/api-docs/**", "/proxy/**", "/actuator/**", "/docs")
+                    .permitAll()
+                .anyRequest()
+                    .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout()
+                .logoutUrl("/api/auth/logout")
+                .addLogoutHandler(logoutHandler)
+                .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext());
+
+        return http.build();
     }
 }

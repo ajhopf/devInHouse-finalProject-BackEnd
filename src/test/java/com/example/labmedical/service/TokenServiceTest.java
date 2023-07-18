9aff58d232c53ab2c18d85a385b4ddae26ec90cc
André Hopf <andre.hopf@gmail.com>
2023-07-18 14:45:39 -0300

    Atualiza testes para TokenService

--- src/test/java/com/example/labmedical/service/TokenServiceTest.java
+++ src/test/java/com/example/labmedical/service/TokenServiceTest.java
@@ -2,10 +2,21 @@ package com.example.labmedical.service;
 
 import com.example.labmedical.repository.TokenRepository;
 import com.example.labmedical.repository.UserRepository;
+import com.example.labmedical.repository.model.User;
 import com.example.labmedical.service.auth.TokenService;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class TokenServiceTest {
@@ -16,27 +27,27 @@ class TokenServiceTest {
     @Mock
     private TokenRepository tokenRepository;
 
-//    @Nested
-//    @DisplayName("Tests of save method")
-//    class saveMethodTests {
-//        @Test
-//        @DisplayName("When user is not found, it should return EntityNotFoundException")
-//        void test1() {
-//            User user = User.builder().id(1L).build();
-//
-//            assertThrows(EntityNotFoundException.class, () -> tokenService.save(Mockito.anyString(), user));
-//        }
-//
-//        @Test
-//        @DisplayName("When user is found, it shouldn't throw an error")
-//        void test2() {
-//            User user = User.builder().id(1L).build();
-//
-//            Mockito.when(userRepository.findById(Mockito.anyLong()))
-//                    .thenReturn(Optional.of(user));
-//
-//            assertDoesNotThrow(() -> tokenService.save(Mockito.anyString(), user));
-//        }
-//    }
+    @Nested
+    @DisplayName("Tests of saveUserToken method")
+    class saveMethodTests {
+        @Test
+        @DisplayName("When user is not found, it should return EntityNotFoundException")
+        void test1() {
+            User user = User.builder().id(1L).build();
+
+            assertThrows(EntityNotFoundException.class, () -> tokenService.saveUserToken(user, Mockito.anyString()));
+        }
+
+        @Test
+        @DisplayName("When user is found, it shouldn't throw an error")
+        void test2() {
+            User user = User.builder().id(1L).build();
+
+            Mockito.when(userRepository.findById(Mockito.anyLong()))
+                    .thenReturn(Optional.of(user));
+
+            assertDoesNotThrow(() -> tokenService.saveUserToken(user, Mockito.anyString()));
+        }
+    }
 
 }

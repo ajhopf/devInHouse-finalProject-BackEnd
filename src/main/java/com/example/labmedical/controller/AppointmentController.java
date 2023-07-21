8b259c2a02e73db4fe6d5ace2640cf8ddd40a75f
André Hopf <andre.hopf@gmail.com>
2023-07-21 15:54:52 -0300

    013 - Adiciona método registerAppointment no appointmentService

--- src/main/java/com/example/labmedical/controller/AppointmentController.java
+++ src/main/java/com/example/labmedical/controller/AppointmentController.java
@@ -1,11 +1,33 @@
 package com.example.labmedical.controller;
 
+import com.example.labmedical.controller.dtos.request.AppointmentRegisterRequest;
+import com.example.labmedical.controller.dtos.response.AppointmentResponse;
+import com.example.labmedical.service.AppointmentService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/consultas")
 @RequiredArgsConstructor
 public class AppointmentController {
+    private final AppointmentService appointmentService;
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<AppointmentResponse> registerAppointment(
+            @RequestBody @Valid AppointmentRegisterRequest request,
+            UriComponentsBuilder uriBuilder
+    ) {
+        AppointmentResponse appointment = appointmentService.registerAppointment(request);
+
+        URI uri = uriBuilder.path("/api/consultas/{id}")
+                .buildAndExpand(appointment.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(appointment);
+    }
 }
--- src/main/java/com/example/labmedical/service/AppointmentService.java
+++ src/main/java/com/example/labmedical/service/AppointmentService.java
@@ -1,6 +1,10 @@
 package com.example.labmedical.service;
 
+import com.example.labmedical.controller.dtos.request.AppointmentRegisterRequest;
+import com.example.labmedical.controller.dtos.response.AppointmentResponse;
+import com.example.labmedical.controller.mapper.AppointmentMapper;
 import com.example.labmedical.repository.AppointmentRepository;
+import com.example.labmedical.repository.model.Appointment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,16 +12,22 @@ import org.springframework.stereotype.Service;
 public class AppointmentService {
     @Autowired
     private AppointmentRepository appointmentRepository;
-//    @Autowired
-//    private AppointmentMapper appointmentMapper;
-//    @Autowired
-//    private PacientService pacientService;
-//
-//    public AppointmentResponse registerAppointment(AppointmentRegisterRequest request) {
-//        Long pacientId = request.getPacientId();
-//        Pacient pacient = pacientService.getPacientById(pacientId);
-//        Appointment appointment = appointmentMapper.map(request);
-////        appointment.setPacient(pacient);
-//
-//    }
+    @Autowired
+    private AppointmentMapper appointmentMapper;
+    @Autowired
+    private PacientService pacientService;
+    @Autowired
+    private LogService logService;
+
+    public AppointmentResponse registerAppointment(AppointmentRegisterRequest request) {
+        pacientService.getPacientById(request.getPacientId());
+
+        Appointment appointment = appointmentMapper.map(request);
+        appointmentRepository.save(appointment);
+
+        logService.success("Consulta registrada. Id consulta: " + appointment.getId() + "; Id Paciente: " + appointment.getPacient().getId());
+
+        return appointmentMapper.map(appointment);
+    }
+
 }

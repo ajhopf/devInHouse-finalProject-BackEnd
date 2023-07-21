9c46d7f68f205e59d0e3d26374fc6b8ba6e38a5d
André Hopf <andre.hopf@gmail.com>
2023-07-21 19:24:15 -0300

    013 - Adiciona testes para método getAppointments

--- src/test/java/com/example/labmedical/service/AppointmentServiceTest.java
+++ src/test/java/com/example/labmedical/service/AppointmentServiceTest.java
@@ -15,6 +15,9 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.times;
 
@@ -30,6 +33,67 @@ class AppointmentServiceTest {
     private LogService logService;
     @InjectMocks
     private AppointmentService appointmentService;
+
+    @Nested
+    @DisplayName("Tests of getAppointments method")
+    class getAppointmentsTests{
+        @Test
+        @DisplayName("When no pacientId is present, it should return all appointments")
+        void test1() {
+            List<Appointment> appointments = List.of(
+                    Appointment.builder().build(),
+                    Appointment.builder().build()
+            );
+
+            List<AppointmentResponse> appointmentResponseList = List.of(
+                    AppointmentResponse.builder().build(),
+                    AppointmentResponse.builder().build()
+            );
+
+            Mockito.when(appointmentRepository.findAll())
+                    .thenReturn(appointments);
+            Mockito.when(appointmentMapper.map(appointments))
+                    .thenReturn(appointmentResponseList);
+
+            List<AppointmentResponse> result = appointmentService.getAppointments(null);
+
+            assertEquals(2, result.size());
+        }
+
+        @Test
+        @DisplayName("When pacientId is present, it should return only that pacients appointments")
+        void test2() {
+            List<Appointment> appointments = List.of(
+                    Appointment.builder().build(),
+                    Appointment.builder().build()
+            );
+
+            List<AppointmentResponse> appointmentResponseList = List.of(
+                    AppointmentResponse.builder().build(),
+                    AppointmentResponse.builder().build()
+            );
+
+            Mockito.when(pacientService.getPacientById(Mockito.anyLong()))
+                    .thenReturn(Pacient.builder().build());
+            Mockito.when(appointmentRepository.getAppointmentsByPacient_Id(Mockito.anyLong()))
+                    .thenReturn(appointments);
+            Mockito.when(appointmentMapper.map(appointments))
+                    .thenReturn(appointmentResponseList);
+
+            List<AppointmentResponse> result = appointmentService.getAppointments(1L);
+
+            assertEquals(2, result.size());
+        }
+
+        @Test
+        @DisplayName("When pacientId is present and there is no pacient with given Id, it should throw EntityNotFoundException")
+        void test3() {
+            Mockito.when(pacientService.getPacientById(Mockito.anyLong()))
+                    .thenThrow(EntityNotFoundException.class);
+
+            assertThrows(EntityNotFoundException.class, () -> appointmentService.getAppointments(1L));
+        }
+    }
 
     @Nested
     @DisplayName("Tests of registerAppointment method")

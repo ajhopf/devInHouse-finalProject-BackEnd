d1fe4a1e229fc6bfc921056861a6fe47881bb47c
jaime nunes <jaimenunesleal@gmail.com>
2023-07-20 23:45:43 -0300

    023 - Alteração code review

--- src/main/java/com/example/labmedical/controller/UserController.java
+++ src/main/java/com/example/labmedical/controller/UserController.java
@@ -10,6 +10,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -36,6 +37,7 @@ public class UserController {
     }
 
     @GetMapping("listar")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<UserResponse>> userGetList(){
         List<UserResponse> response = userService.getListUsers();
         return  ResponseEntity.status(HttpStatus.OK).body(response);
@@ -50,6 +52,7 @@ public class UserController {
     }
 
     @GetMapping("buscar/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<UserResponse> userSearch(
             @Valid @PathVariable Long id
     ){

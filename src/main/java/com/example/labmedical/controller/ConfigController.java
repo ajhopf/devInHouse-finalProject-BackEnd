a7d8c1d1c973c60f55b269306efd1b680575eaba
Leandro-da Silva-Michail-Krikis <leandro_silva22@estudante.sesisenai.org.br>
2023-07-22 16:39:48 -0300

    Criação do ConfigController

--- src/main/java/com/example/labmedical/controller/ConfigController.java
+++ src/main/java/com/example/labmedical/controller/ConfigController.java
@@ -1,18 +1,29 @@
 package com.example.labmedical.controller;
 
+import com.example.labmedical.controller.dtos.request.SystemConfigRequest;
 import com.example.labmedical.service.ConfigService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/config")
+
 public class ConfigController {
     @Autowired
     private ConfigService configService;
 
+    @PostMapping ("/sistema")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Void> createSystem(
+           @Valid @RequestBody SystemConfigRequest config
+    ) throws JsonProcessingException {
+        configService.saveSystemConfig(config);
+        return ResponseEntity.ok().build();
+    }
 
 }

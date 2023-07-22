87073f6898fab27dc28c71cb8ebfb8f1dca8d125
Leandro-da Silva-Michail-Krikis <leandro_silva22@estudante.sesisenai.org.br>
2023-07-22 16:38:13 -0300

    Criação de 3 usuarios admin, doctor e nurse evitendo as exception de insert

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,29 @@
-INSERT INTO USERS (NAME, EMAIL, PASSWORD, PHOTO_URL, ROLE)
-VALUES ('André', 'andre.hopf@hotmail.com', '12345', 'photo', 'ROLE_ADMIN');
+-- DROP TABLE TOKEN;
+-- DROP TABLE USERS;
+-- DROP TABLE CONFIG;
+-- DROP TABLE ALERGIES;
+-- DROP TABLE APPOINTMENTS;
+-- DROP TABLE SPECIAL_CARE;
+-- DROP TABLE LOG;
+-- DROP TABLE pacients;
+-- DROP TABLE addresses;
+MERGE INTO USERS U
+    USING DUAL
+    ON (U.EMAIL = 'admin@gmail.com')
+    WHEN NOT MATCHED THEN
+        INSERT (NAME, EMAIL, PASSWORD, PHOTO_URL, ROLE)
+            VALUES ('Admin', 'admin@gmail.com', '12345', null, 'ROLE_ADMIN');
+
+MERGE INTO USERS U
+    USING DUAL
+    ON (U.EMAIL = 'doctor@gmail.com')
+    WHEN NOT MATCHED THEN
+        INSERT (NAME, EMAIL, PASSWORD, PHOTO_URL, ROLE)
+            VALUES ('Doctor', 'doctor@gmail.com', '12345', null, 'ROLE_DOCTOR');
+
+MERGE INTO USERS U
+    USING DUAL
+    ON (U.EMAIL = 'nurse@gmail.com')
+    WHEN NOT MATCHED THEN
+        INSERT (NAME, EMAIL, PASSWORD, PHOTO_URL, ROLE)
+            VALUES ('Nurse', 'nurse@gmail.com', '12345', null, 'ROLE_NURSE');

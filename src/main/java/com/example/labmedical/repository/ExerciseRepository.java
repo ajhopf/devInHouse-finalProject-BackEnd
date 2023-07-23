792952f03c06e84c14eae7c6d5193d4bb8ebe834
Leandro-da Silva-Michail-Krikis <leandro_silva22@estudante.sesisenai.org.br>
2023-07-23 17:17:43 -0300

    Criação do ExerciseRepository

---
+++ src/main/java/com/example/labmedical/repository/ExerciseRepository.java
@@ -0,0 +1,9 @@
+package com.example.labmedical.repository;
+
+import com.example.labmedical.repository.model.Exercise;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
+}

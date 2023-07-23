f9a19255503fbb13cd17ed93ecb6dc541d341da9
Leandro-da Silva-Michail-Krikis <leandro_silva22@estudante.sesisenai.org.br>
2023-07-23 17:16:17 -0300

    Criação do model Exercise

---
+++ src/main/java/com/example/labmedical/repository/model/Exercise.java
@@ -0,0 +1,37 @@
+package com.example.labmedical.repository.model;
+
+import com.example.labmedical.enums.ExerciseType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Table(name = "exercises")
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Exercise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    private ExerciseType exerciseType;
+    private Integer timesPerWeek;
+    private LocalDate dateCreated;
+    private LocalTime timeCreated;
+    private String description;
+    private boolean status;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User createdByUser;
+    @ManyToOne
+    @JoinColumn(name = "patient_id")
+    private Pacient patient;
+}

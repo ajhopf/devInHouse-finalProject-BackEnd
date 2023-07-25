c5756a2e3334091d8e8710c15a057f33c4859695
Leandro-da Silva-Michail-Krikis <leandro_silva22@estudante.sesisenai.org.br>
2023-07-25 20:29:21 -0300

    Correção da descrição dos erros

--- src/main/java/com/example/labmedical/controller/dtos/response/ExerciseResponse.java
+++ src/main/java/com/example/labmedical/controller/dtos/response/ExerciseResponse.java
@@ -3,11 +3,13 @@ package com.example.labmedical.controller.dtos.response;
 import com.example.labmedical.enums.ExerciseType;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 @Data
+@Builder
 public class ExerciseResponse {
     private Long id;
     private String description;
